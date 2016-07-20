72612faaf520671f0a8d93fd5653db931157b782
hilaryml <hilyttle@gmail.com>
2016-07-20 12:42:02 -0400

    adding nokogiri to scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,16 +5,19 @@ require 'pry'
 class Scraper
 
 	def scrape_pokemon_index
+		
 		doc = Nokogiri::HTML(open("http://pokemon.wikia.com/wiki/Category:Generation_I_Pok%C3%A9mon"))
-		binding.pry
+		#binding.pry
 		all_pokemon_array = []
-
-		doc.css().each {|pokemon|
-			each_pokemon_hash = {number: ,
-			name: ,
-			type:
+		count = 0
+		doc.css("div.lightbox-caption").each {|pokemon|
+			
+		    each_pokemon_hash = {number: count + 1,
+			name: pokemon.css("a").attribute("title").text,
+			type: pokemon.css("a span").text
 			}
 			all_pokemon_array << each_pokemon_hash
+		    count += 1
 		}
 		all_pokemon_array
 
@@ -22,26 +25,26 @@ class Scraper
 
 	def scrape_pokedex_entry(entry_url)
 
-		entry = Nokogiri::HTML(open(entry_url))
+		#entry = Nokogiri::HTML(open(entry_url))
 
-		entry_hash = {}
+		#entry_hash = {}
 
-		entry.css().each {|category|
-			case category
+		#entry.css().each {|category|
+		#	case category
 
-			when "Physiology"
-				entry_hash[:physiology] = 
-			when "Natural Abilities"
-				entry_hash[:natural_abilities] =
-			when "Behaviour"
-				entry_hash[:behaviour] = 
-			when "Habitat"
-				entry_hash[:habitat] =
-			when "Evolution"
-				entry_hash[:evolution] =
-			end
-		}
-		entry_hash
+		#	when "Physiology"
+		#		entry_hash[:physiology] = 
+		#	when "Natural Abilities"
+		#		entry_hash[:natural_abilities] =
+		#	when "Behaviour"
+		#		entry_hash[:behaviour] = 
+		#	when "Habitat"
+		#		entry_hash[:habitat] =
+		#	when "Evolution"
+		#		entry_hash[:evolution] =
+		#	end
+		#}
+		#entry_hash
 	end
 
 end
