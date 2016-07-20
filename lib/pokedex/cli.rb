13153df783c9b8de87b1100c2081eecceacabac5
hilaryml <hilyttle@gmail.com>
2016-07-19 21:10:10 -0400

    more cli methods - basic

--- lib/pokedex/cli.rb
+++ lib/pokedex/cli.rb
@@ -18,6 +18,34 @@ class Pokedex::CLI
 4. Charazard - Fire"
 	end
 
+	def menu
+		puts "Which Pokemon would you like to know more about? (number) Type 'exit' to leave the program."
+		input = nil
+		while input != "exit"
+			puts "Type the number of the Pokemon that you would like to know more about. Type 'list' to see the Pokemon again. Type 'exit' to leave the program."
+			input = gets.strip.downcase
+		
+			case input
+			when "1"
+				puts "Bulbasaur info"
+			when "2"
+				puts "Ivysaur info"
+			when "3"
+				puts "Venosaur info"
+			when "4"
+				puts "Charazard info"
+			when "list"
+				list_pokemon
+			else
+				puts "Invalid entry. Try again."
+			end
+		end
+	end
+
+	def bye
+		puts "Good luck, Pokemon Trainer!"
+	end
+	
 end
 
 #to-do: use nokogiri and open-uri to scrape pages for attributes and save in array of hashes
