ad705cd5b27e2f4b718c8d91ee46dc8b16bb9bb1
hilaryml <hilyttle@gmail.com>
2016-07-20 18:31:45 -0400

    adding nokogiri to scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,48 +5,58 @@ require 'pry'
 class Scraper
 
 	def scrape_pokemon_index
-		
+
 		doc = Nokogiri::HTML(open("http://pokemon.wikia.com/wiki/Category:Generation_I_Pok%C3%A9mon"))
 		#binding.pry
-		all_pokemon_array = []
-		count = 0
-		doc.css("div.lightbox-caption").each {|pokemon|
+		#all_pokemon_array = []
+		#count = 0
+		#doc.css("div.lightbox-caption a").each {|pokemon|
 			
-		    each_pokemon_hash = {number: count + 1,
-			name: pokemon.css("a").attribute("title").text,
-			type: pokemon.css("a span").text
-			}
-			all_pokemon_array << each_pokemon_hash
-		    count += 1
-		}
-		all_pokemon_array
+		#	each_pokemon_hash = {number: count + 1,
+		#	name: pokemon.attribute("title").text,
+		#	type: pokemon.css("span").text}
+		#	entry_url: "http://pokemon.wikia.com#{pokemon.attribute("href").text}"
+		#	}
+		#	all_pokemon_array << each_pokemon_hash
+		#	count += 1
+		#}
+		#all_pokemon_array
 
 	end
 
 	def scrape_pokedex_entry(entry_url)
 
-		#entry = Nokogiri::HTML(open(entry_url))
+		entry = Nokogiri::HTML(open(entry_url))
+		binding.pry
+		entry_hash = {}
+		
+		entry.css("h3").each_with_index {|section, index|
+		section = section.text
 
-		#entry_hash = {}
+		case section
 
-		#entry.css().each {|category|
-		#	case category
-
-		#	when "Physiology"
-		#		entry_hash[:physiology] = 
-		#	when "Natural Abilities"
-		#		entry_hash[:natural_abilities] =
-		#	when "Behaviour"
-		#		entry_hash[:behaviour] = 
-		#	when "Habitat"
-		#		entry_hash[:habitat] =
-		#	when "Evolution"
-		#		entry_hash[:evolution] =
-		#	end
-		#}
-		#entry_hash
+		when "Physiology"
+			entry_hash[:physiology] = entry.css("h3~p")[index].text
+		when "Natural Abilities"
+			entry_hash[:natural_abilities] = entry.css("h3~p")[index].text
+		when "Behavior"
+			entry_hash[:behavior] = entry.css("h3~p")[index].text
+		when "Habitat"
+			entry_hash[:habitat] = entry.css("h3~p")[index].text
+		end
+		
+		}
+		entry_hash
 	end
 
 end
 
-Scraper.new.scrape_pokemon_index
+#Scraper.new.scrape_pokemon_index
+Scraper.new.scrape_pokedex_entry("http://pokemon.wikia.com/wiki/Bulbasaur")
+
+#entry.css("h3").text printed the names of each h3 heading
+#entry.css("h3~p").text this selected all of the text under the h2 headers
+#key-value pairs? 
+
+#remember to weed out unnecessary categories... case statement?
+
